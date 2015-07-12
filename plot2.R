b0969d8dec4a6889a95a40e70eb3fa4f7724a534
hayne <ebuschang@gmail.com>
2015-07-11 22:37:42 -0500

    first pass at assignment 1 - partially compplete

--- plot2.R
+++ plot2.R
@@ -1,1 +1,49 @@
 # produces a line graph of global active power
+# # produces a bar graph of global active power
+
+# getData <- function() {
+require("data.table")
+library("data.table")
+
+file_name <- "household_power_consumption.txt"
+
+# read in the file
+#
+if (!file.exists(file_name)){
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(url,"asgn1.zip",method="curl")
+    file_names <- unzip("asgn1.zip",list=TRUE)
+
+    if (file_names["Name"] != file_name) {
+        stop("File name not found.")
+    }
+    unzip("asgn1.zip")
+}
+
+# read subset of data
+df <- fread(file_name, sep=";",na.strings="?")#,colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+start_date <- "1/2/2007"
+end_date <- "2/2/2007"
+
+df.subset <- subset(df, Date == start_date | Date == end_date)
+
+df.subset$Date = as.Date(df.subset$Date,format="%d/%m/%Y")
+df.subset$Time = strptime(df.subset$Time, format = "%H:%M:%S")
+
+# }
+#
+# gather file data
+# getData
+
+# plot graph
+png('plot2.png')
+hist(as.numeric(df.subset$Global_active_power),
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency",
+     col="red")
+print(as.numeric(df.subset$Global_active_power))
+dev.off()
+# save graph as plot1.PNG
+
--- plot4.R
+++ plot4.R
@@ -1,5 +1,56 @@
-# produces 4 line graphs
+# produces 4 line graphs on power data
 # 1. global active power
 # 2. voltage
 # 3. energy submetering
 # 4. global reactive power
+#
+# # produces a bar graph of global active power
+
+# getData <- function() {
+require("data.table")
+library("data.table")
+
+file_name <- "household_power_consumption.txt"
+
+# read in the file
+#
+if (!file.exists(file_name)){
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(url,"asgn1.zip",method="curl")
+    file_names <- unzip("asgn1.zip",list=TRUE)
+
+    if (file_names["Name"] != file_name) {
+        stop("File name not found.")
+    }
+    unzip("asgn1.zip")
+}
+
+# read subset of data
+df <- fread(file_name, sep=";",na.strings="?")
+
+start_date <- "1/2/2007"
+end_date <- "2/2/2007"
+
+df.subset <- subset(df, Date == start_date | Date == end_date)
+
+df.subset$Date = as.Date(df.subset$Date,format="%d/%m/%Y")
+df.subset$Time = strptime(df.subset$Time, format = "%H:%M:%S")
+
+# plot 4 graphs
+png('plot4.png')
+par(mfrow=c(2,2))
+
+# top left
+hist(as.numeric(df.subset$Global_active_power),
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency",
+     col="red")
+# top right
+
+# bottom left
+
+# bottom right
+
+#
+dev.off()
